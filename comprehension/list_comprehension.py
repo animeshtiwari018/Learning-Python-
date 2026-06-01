1902030a55c5a358f53ca3826aaa7cef3b1afcaf
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-06-01 15:33:59 +0530

    list comprehension

---
+++ comprehension/list_comprehension.py
@@ -0,0 +1,11 @@
+officers = [
+    "Brig Saurabh Singh Sekhawat",
+    "Major Sudhir Walia",
+    "Col Panging",
+    "Col Kaushal Kashyap"
+
+]
+
+Officers_loop = [officers for officers in officers if "Brig" in officers]
+
+print(Officers_loop)
